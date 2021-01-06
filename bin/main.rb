4e7d9eaca92c04db845cc31a698bc01b293230e3
abeidahmed <abeidahmed92@gmail.com>
2021-01-06 14:53:33 +0530

    Check if player entered the same name

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,36 @@
 #!/usr/bin/env ruby
 
-puts 'Hello world!'
+def register_player_for(player_name)
+  puts ''
+  puts "Please enter Player #{player_name}'s name"
+
+  player_name = gets.chomp.strip
+
+  while player_name.empty?
+    puts 'Please enter a valid name'
+    player_name = gets.chomp
+  end
+
+  player_name
+end
+
+puts ''
+puts 'Welcome to Tic Tac Toe'
+puts ''
+puts 'Before we begin with the game, register yourself and your partner'
+puts "Press 'Enter' to continue or press 'q' to exit the game"
+
+command = gets.chomp.strip
+exit if command == 'q'
+
+puts ''
+
+player_x = register_player_for('X')
+player_o = register_player_for('O')
+
+while player_x.downcase == player_o.downcase
+  puts ''
+  puts "Players should have different names. Enter a different name for 'Player O'"
+  puts ''
+  player_o = register_player_for('O')
+end
