922693dceceec50941daa80340841116faf269e2
abeidahmed <abeidahmed92@gmail.com>
2021-01-06 22:16:08 +0530

    Add more tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,3 @@
-require 'byebug'
 require_relative 'spec_helper'
 require_relative '../lib/game'
 require_relative '../lib/player'
@@ -12,23 +11,20 @@ RSpec.describe Game do
 
   describe '#initialize' do
     it 'sets the necessary instance variables' do
-      expect(game.instance_variable_get(:@player_x)).to eq(player_x)
-      expect(game.instance_variable_get(:@player_o)).to eq(player_o)
+      expect(game.instance_variable_get(:@player_x)).to eq('John')
+      expect(game.instance_variable_get(:@player_o)).to eq('Maggie')
       expect(game.instance_variable_get(:@board)).to be_a(Board)
       expect(game.instance_variable_get(:@total_rounds)).to be_zero
       expect(game.instance_variable_get(:@game_over)).to eq(board.game_over)
       expect(game.instance_variable_get(:@someone_won)).to eq(false)
+      expect(game.instance_variable_get(:@show_message)).to eq('')
     end
   end
 
   describe '#current_turn_to_play' do
     it 'returns x turn to play when player_1 should play' do
-      expect(game.current_turn_to_play).to eq("John's turn (x)")
+      expect(game.current_turn_to_play).to eq("\nJohn's turn (x): ")
     end
-
-    # xit 'returns o turn to play when player_2 should play' do
-    #   expect(game.current_turn_to_play).to eq("Maggie's turn (o)")
-    # end
   end
 
   describe '#make_play' do
@@ -36,6 +32,24 @@ RSpec.describe Game do
       game.make_play(1)
 
       expect(game.instance_variable_get(:@total_rounds)).to eq(1)
+    end
+
+    it 'should set @game_over to true and @someone_won to false if it was a draw' do
+      game.instance_variable_set(:@total_rounds, 8)
+      game.make_play(1)
+
+      expect(game.instance_variable_get(:@game_over)).to be_truthy
+      expect(game.instance_variable_get(:@someone_won)).to be_falsy
+    end
+  end
+
+  describe '#win_message' do
+    it 'returns the player name who won' do
+      expect(game.win_message(true)).to eq('Maggie won')
+    end
+
+    it 'returns nil if arg is false' do
+      expect(game.win_message(false)).to be_nil
     end
   end
 end
