4af3f959d07b66d7f5f8fbad49ffc6fa7601f6b3
abeidahmed <abeidahmed92@gmail.com>
2021-01-06 15:17:22 +0530

    Show game board and instructions

--- bin/main.rb
+++ bin/main.rb
@@ -3,7 +3,6 @@
 EXIT_KEY = 'q'.freeze
 
 def register_player_for(player_name)
-  puts ''
   puts "Please enter Player #{player_name}'s name"
 
   player_name = gets.chomp.strip
@@ -28,6 +27,7 @@ exit if command == EXIT_KEY
 puts ''
 
 player_x = register_player_for('X')
+puts ''
 player_o = register_player_for('O')
 
 while player_x.downcase == player_o.downcase
@@ -36,3 +36,18 @@ while player_x.downcase == player_o.downcase
   puts ''
   player_o = register_player_for('O')
 end
+
+puts ''
+puts 'Game instructions'
+puts ''
+puts 'Type your desired position from 1 to 9 as shown below'
+puts ''
+puts '***************'
+puts '| |1| |2| |3| |'
+puts '***************'
+puts '| |4| |5| |6| |'
+puts '***************'
+puts '| |7| |8| |9| |'
+puts '***************'
+puts ''
+puts ''
