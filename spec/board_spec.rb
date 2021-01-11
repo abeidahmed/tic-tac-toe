c0b87cc32d03aee787433aa17445893433670ece
abeidahmed <abeidahmed92@gmail.com>
2021-01-11 14:37:09 +0530

    Add tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,67 +1,67 @@
-# require_relative 'spec_helper'
-# require_relative '../lib/board'
+require_relative 'spec_helper'
+require_relative '../lib/board'
 
-# # rubocop:disable Metrics/BlockLength
-# RSpec.describe Board do
-#   let(:board) { Board.new }
+# rubocop:disable Metrics/BlockLength
+RSpec.describe Board do
+  let(:board) { Board.new }
 
-#   describe '#initialize' do
-#     it 'should set the initial instance variables' do
-#       expect(board.instance_variable_get(:@board)).to eq([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-#       expect(board.instance_variable_get(:@player_x_turn)).to eq(true)
-#       expect(board.instance_variable_get(:@game_over)).to eq(false)
-#       expect(board.instance_variable_get(:@player_x_selections)).to match_array([])
-#       expect(board.instance_variable_get(:@player_o_selections)).to match_array([])
-#     end
-#   end
+  describe '#initialize' do
+    it 'should set the initial instance variables' do
+      expect(board.instance_variable_get(:@board)).to eq([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+      expect(board.instance_variable_get(:@player_x_turn)).to eq(true)
+      expect(board.instance_variable_get(:@game_over)).to eq(false)
+      expect(board.instance_variable_get(:@player_x_selections)).to match_array([])
+      expect(board.instance_variable_get(:@player_o_selections)).to match_array([])
+    end
+  end
 
-#   describe '#play_turn_and_swap' do
-#     it 'returns false if the selection is not valid' do
-#       expect(board.play_turn_and_swap(10)).to be_falsy
-#     end
+  describe '#play_turn_and_swap' do
+    it 'returns false if the selection is not valid' do
+      expect(board.play_turn_and_swap(10)).to be_falsy
+    end
 
-#     it 'switches player turns' do
-#       board.instance_variable_set(:@player_x_turn, false)
-#       board.play_turn_and_swap(1)
+    it 'switches player turns' do
+      board.instance_variable_set(:@player_x_turn, false)
+      board.play_turn_and_swap(1)
 
-#       expect(board.instance_variable_get(:@player_x_turn)).to be_truthy
-#     end
+      expect(board.instance_variable_get(:@player_x_turn)).to be_truthy
+    end
 
-#     it 'returns false if the position is already taken and the player makes a selection' do
-#       board.instance_variable_set(:@board, [['x', 2, 3], [4, 5, 6], [7, 8, 9]])
+    it 'returns false if the position is already taken and the player makes a selection' do
+      board.instance_variable_set(:@board, [['x', 2, 3], [4, 5, 6], [7, 8, 9]])
 
-#       expect(board.play_turn_and_swap(1)).to be_falsy
-#     end
+      expect(board.play_turn_and_swap(1)).to be_falsy
+    end
 
-#     it 'returns true if the postion is available' do
-#       expect(board.play_turn_and_swap(1)).to be_truthy
-#     end
+    it 'returns true if the postion is available' do
+      expect(board.play_turn_and_swap(1)).to be_truthy
+    end
 
-#     it 'sets the board with (x) if it is player_1 turn' do
-#       board.instance_variable_set(:@player_x_turn, true)
-#       board.play_turn_and_swap(1)
+    it 'sets the board with (x) if it is player_1 turn' do
+      board.instance_variable_set(:@player_x_turn, true)
+      board.play_turn_and_swap(1)
 
-#       expect(board.instance_variable_get(:@board)).to match_array([['x', 2, 3], [4, 5, 6], [7, 8, 9]])
-#       expect(board.instance_variable_get(:@player_x_selections)).to match_array([1])
-#     end
+      expect(board.instance_variable_get(:@board)).to match_array([['x', 2, 3], [4, 5, 6], [7, 8, 9]])
+      expect(board.instance_variable_get(:@player_x_selections)).to match_array([1])
+    end
 
-#     it 'sets the board with (o) if it is player_2 turn' do
-#       board.instance_variable_set(:@player_x_turn, false)
-#       board.play_turn_and_swap(1)
+    it 'sets the board with (o) if it is player_2 turn' do
+      board.instance_variable_set(:@player_x_turn, false)
+      board.play_turn_and_swap(1)
 
-#       expect(board.instance_variable_get(:@board)).to match_array([['o', 2, 3], [4, 5, 6], [7, 8, 9]])
-#       expect(board.instance_variable_get(:@player_o_selections)).to match_array([1])
-#     end
+      expect(board.instance_variable_get(:@board)).to match_array([['o', 2, 3], [4, 5, 6], [7, 8, 9]])
+      expect(board.instance_variable_get(:@player_o_selections)).to match_array([1])
+    end
 
-#     it 'sets @game_over to true if there is a win pattern' do
-#       board.play_turn_and_swap(1) # x turn
-#       board.play_turn_and_swap(4) # y turn
-#       board.play_turn_and_swap(2) # x turn
-#       board.play_turn_and_swap(5) # y turn
-#       board.play_turn_and_swap(3) # x turn
+    it 'sets @game_over to true if there is a win pattern' do
+      board.play_turn_and_swap(1) # x turn
+      board.play_turn_and_swap(4) # y turn
+      board.play_turn_and_swap(2) # x turn
+      board.play_turn_and_swap(5) # y turn
+      board.play_turn_and_swap(3) # x turn
 
-#       expect(board.instance_variable_get(:@game_over)).to be_truthy
-#     end
-#   end
-# end
-# # rubocop:enable Metrics/BlockLength
+      expect(board.instance_variable_get(:@game_over)).to be_truthy
+    end
+  end
+end
+# rubocop:enable Metrics/BlockLength
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,57 +1,57 @@
-# require_relative 'spec_helper'
-# require_relative '../lib/game'
-# require_relative '../lib/player'
-# require_relative '../lib/board'
+require_relative 'spec_helper'
+require_relative '../lib/game'
+require_relative '../lib/player'
+require_relative '../lib/board'
 
-# # rubocop:disable Metrics/BlockLength
-# RSpec.describe Game do
-#   let(:player_x) { Player.new('John', 'x') }
-#   let(:player_o) { Player.new('Maggie', 'o') }
-#   let(:board) { Board.new }
-#   let(:game) { Game.new(player_x, player_o) }
+# rubocop:disable Metrics/BlockLength
+RSpec.describe Game do
+  let(:player_x) { Player.new('John', 'x') }
+  let(:player_o) { Player.new('Maggie', 'o') }
+  let(:board) { Board.new }
+  let(:game) { Game.new(player_x, player_o) }
 
-#   describe '#initialize' do
-#     it 'sets the necessary instance variables' do
-#       expect(game.instance_variable_get(:@player_x)).to eq('John')
-#       expect(game.instance_variable_get(:@player_o)).to eq('Maggie')
-#       expect(game.instance_variable_get(:@board)).to be_a(Board)
-#       expect(game.instance_variable_get(:@total_rounds)).to be_zero
-#       expect(game.instance_variable_get(:@game_over)).to eq(board.game_over)
-#       expect(game.instance_variable_get(:@someone_won)).to eq(false)
-#       expect(game.instance_variable_get(:@show_message)).to eq('')
-#     end
-#   end
+  describe '#initialize' do
+    it 'sets the necessary instance variables' do
+      expect(game.instance_variable_get(:@player_x)).to eq('John')
+      expect(game.instance_variable_get(:@player_o)).to eq('Maggie')
+      expect(game.instance_variable_get(:@board)).to be_a(Board)
+      expect(game.instance_variable_get(:@total_rounds)).to be_zero
+      expect(game.instance_variable_get(:@game_over)).to eq(board.game_over)
+      expect(game.instance_variable_get(:@someone_won)).to eq(false)
+      expect(game.instance_variable_get(:@show_message)).to eq('')
+    end
+  end
 
-#   describe '#current_turn_to_play' do
-#     it 'returns x turn to play when player_1 should play' do
-#       expect(game.current_turn_to_play).to match(/John/)
-#     end
-#   end
+  describe '#current_turn_to_play' do
+    it 'returns x turn to play when player_1 should play' do
+      expect(game.current_turn_to_play).to match(/John/)
+    end
+  end
 
-#   describe '#make_play' do
-#     it 'should increment @total_rounds by 1' do
-#       game.make_play(1)
+  describe '#make_play' do
+    it 'should increment @total_rounds by 1' do
+      game.make_play(1)
 
-#       expect(game.instance_variable_get(:@total_rounds)).to eq(1)
-#     end
+      expect(game.instance_variable_get(:@total_rounds)).to eq(1)
+    end
 
-#     it 'should set @game_over to true and @someone_won to false if it was a draw' do
-#       game.instance_variable_set(:@total_rounds, 8)
-#       game.make_play(1)
+    it 'should set @game_over to true and @someone_won to false if it was a draw' do
+      game.instance_variable_set(:@total_rounds, 8)
+      game.make_play(1)
 
-#       expect(game.instance_variable_get(:@game_over)).to be_truthy
-#       expect(game.instance_variable_get(:@someone_won)).to be_falsy
-#     end
-#   end
+      expect(game.instance_variable_get(:@game_over)).to be_truthy
+      expect(game.instance_variable_get(:@someone_won)).to be_falsy
+    end
+  end
 
-#   describe '#win_message' do
-#     it 'returns the player name who won' do
-#       expect(game.win_message(true)).to match(/Maggie/)
-#     end
+  describe '#win_message' do
+    it 'returns the player name who won' do
+      expect(game.win_message(true)).to match(/Maggie/)
+    end
 
-#     it 'returns nil if arg is false' do
-#       expect(game.win_message(false)).to be_nil
-#     end
-#   end
-# end
-# # rubocop:enable Metrics/BlockLength
+    it 'returns nil if arg is false' do
+      expect(game.win_message(false)).to be_nil
+    end
+  end
+end
+# rubocop:enable Metrics/BlockLength
